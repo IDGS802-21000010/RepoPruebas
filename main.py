2a356c7748bb624a595b84ef675af23bb94e1a0e
IDSGS802-21000010 <gaboguerrero2024@gmail.com>
2024-05-22 13:19:46 -0600

    primer commit

---
+++ main.py
@@ -0,0 +1,18 @@
+import random
+from collections import Counter
+
+lista = []
+
+for i in range(1000):
+    lista.append(random.randint(1,100))
+
+c = Counter(lista)
+nR = c.most_common(1)[0][0]
+nU = min(c, key=c.get)
+
+listaO = sorted(lista)
+print(listaO)
+print(nR)
+print(nU)
+print(lista)
+
